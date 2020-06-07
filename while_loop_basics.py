dc5dd6367572c103fc3a8e17d95290e1c346d5f3
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-06-07 00:28:01 -0700

    Add files via upload

---
+++ while_loop_basics.py
@@ -0,0 +1,25 @@
+while True:
+    toppings = str(input("Please enter a toppings: "))
+    if toppings == "X":
+        print ("Exiting the program!")
+        break
+    else:
+        print ("Adding " + (toppings) + " to your pizza!" + "\n")
+
+##while True:
+##    age = input("Please enter your age or type quit to exit: ")
+####    print (type(age))
+##    if (age == "quit"):
+##        print "Exiting the program!"
+##        break
+##
+##    if (age < 0):
+##        print "Invalid entry!"
+##    elif (age < 3):
+##        print "Free admission!"
+##    elif (3 <= age < 12):
+##        print "Tickets cost $10."
+##    elif (age >= 12):
+##        print "Tickets cost $15."
+    
+    
