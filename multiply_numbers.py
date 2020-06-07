286bb104747c1d4c8bcc77a7b4d4edc7695ec2c6
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-06-07 00:11:25 -0700

    Add files via upload

---
+++ multiply_numbers.py
@@ -0,0 +1,20 @@
+####Add numbers in a range#####
+
+number_list = [3,4,2,1,2,7,8]
+
+
+total = 0
+for x in (number_list):
+    total = total + x
+    print(total)
+
+####Multiply numbers in a range#####
+number = 1
+
+for y in number_list:
+    number = number*y
+    print(number)
+    
+        
+        
+        
