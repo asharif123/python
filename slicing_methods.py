21cc161bda916b87b2e5eec15c2d736758107855
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-06-07 00:39:11 -0700

    Add files via upload

---
+++ slicing_methods.py
@@ -0,0 +1,12 @@
+my_list = [99,4,2,5,-3]
+my_tuple = (99,4,2,5,-3)
+my_str = "sequoia"
+print(my_list[:])
+# output: [99,4,2,5,-3]
+print(my_tuple[1:])
+# output: (4,2,5,-3)
+print(my_str[:3])
+# output: "seq"
+print(my_tuple[2:4])
+# output: (2,5)
+print(my_list, my_tuple, my_str)
