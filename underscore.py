79027c8c73dbb76d89f2e028798861be014a7b1a
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-06-07 00:39:26 -0700

    Add files via upload

---
+++ underscore.py
@@ -0,0 +1,16 @@
+##underscore optional
+##Iterable will be the list being passed in
+##callback will be the lambda function.
+class Underscore:
+    def map(self, iterable, callback):
+        for i in range(len(iterable)):
+            
+    def find(self, iterable, callback):
+        # your code here
+    def filter(self, iterable, callback):
+        # your code
+    def reject(self, iterable, callback):
+        # your code
+# you just created a library with 4 methods!
+# let's create an instance of our class
+_ = Underscore() # yes we are setting our instance to a variable that is an underscore
