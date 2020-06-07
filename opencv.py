4d16053cd88935a6731d668448a12843e0e45ef5
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-06-07 00:38:40 -0700

    Add files via upload

---
+++ opencv.py
@@ -0,0 +1,4 @@
+import opencv
+##OpenCV uses machine learning algorithms to search for faces within a picture
+#algorithms break the task of identifying the face into thousands of smaller, bite-sized tasks,
+#cascades themselves are just a bunch of XML files that contain OpenCV data used to detect objects
